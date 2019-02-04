3e18150bd340ea040f241706487310dc5b520fe5
paulo <pauloh16.pdr@gmail.com>
2019-02-04 14:26:58 -0200

    Finalizar desafio 2

--- desafio 2/script.js
+++ desafio 2/script.js
@@ -1,3 +1,10 @@
-let lista = document.querySelector ("body>div");
-let caixaDeTexto = document.querySelector ("input");
+document.getElementById("adicionar").addEventListener("click", function(){
+    let lista=document.getElementById("lista");
+    let itemNovo=document.getElementById("itemNovo");
+    let item=document.createElement("li");
 
+    item.textContent=itemNovo.value;
+    lista.appendChild(item);
+    itemNovo.value="";
+    itemNovo.focus();
+})
